c66cff47a082b825fa1b467aec35182a9115f403
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-05 19:59:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const micBtn = document.getElementById("micBtn");
 
+micBtn.addEventListener("click", () => {
+  const command = prompt("Say a command (Add job / Dashboard)");
+
+  if (!command) return;
+
+  const text = command.toLowerCase();
+
+  if (text.includes("add job")) {
+    window.location.href = "job.html";
+  } else if (text.includes("dashboard")) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Command not recognised");
+  }
+});
