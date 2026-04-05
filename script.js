147cbd11e47e2e11d3d1ba36a9a815209341ed3d
adamgreenwood895-lang <adam.greenwood895@gmail.com>
2026-04-05 22:39:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
 function handleTap() {
-  console.log("Mic tapped");
-
-  const zone = document.querySelector(".tap-zone");
-
-  zone.style.boxShadow = "0 0 60px rgba(255,165,0,0.9)";
-
-  setTimeout(() => {
-    zone.style.boxShadow = "none";
-  }, 300);
-
-  alert("Listening...");
+  alert("Mic working 🎤");
 }
